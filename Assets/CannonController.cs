1885391d66ba5458360c31f046c321067eb6d805
Sergio Monroy Escalante <sergio.monroy.escalante@gmail.com>
2018-11-19 14:33:20 -0500

    push the chanllenge

---
+++ Assets/CannonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour {
+    public float rotationSpeed;
+    public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+        this.transform.Rotate(new Vector3(0, rotationSpeed, 0));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.UpArrow) && transform.rotation.eulerAngles.x > 270) {
+            this.transform.Rotate(new Vector3(0, rotationSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.rotation.eulerAngles.x < 350) {
+            this.transform.Rotate(new Vector3(0, -rotationSpeed, 0));
+        }
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            var bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.AngleAxis(90, new Vector3(1, 0, 0)));
+            bullet.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+            bullet.GetComponent<Rigidbody2D>().velocity = transform.forward * 10;
+            Destroy(bullet, 3);
+        }
+    }
+}
---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour {
+
+    public GameObject blockPrefab;
+    public float blockHeight;
+    public float blockWidth;
+    public float blockMargen;
+    public float towerHorizontalPosition;
+    public Color color1;
+    public Color color2;
+    public float blockSpeed;
+
+    private float currentPosition;
+    private float currentWidth;
+    private Stack<GameObject> tower;
+    private GameObject movingBlock;
+    private bool movingRight;
+
+	// Use this for initialization
+	void Start () {
+        tower = new Stack<GameObject>();
+        movingRight = false;
+        RestartTower();
+        GetNewBlock();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            float diff = tower.Peek().transform.position.x - movingBlock.transform.position.x;
+            currentWidth -= Mathf.Abs(diff);
+            if (currentWidth < 0) {
+                RestartTower();
+            } else {
+                movingBlock.transform.localScale = new Vector3(currentWidth, blockHeight, 1);
+                movingBlock.transform.position += new Vector3(diff / 2, 0, 0);
+                movingBlock.GetComponent<Renderer>().material.color = color1;
+                tower.Push(movingBlock);
+            }
+            GetNewBlock();
+        }
+    }
+
+    void FixedUpdate () {
+        if(movingBlock != null) {
+            if (movingRight && movingBlock.transform.position.x > tower.Peek().transform.position.x + currentWidth + blockMargen) {
+                movingRight = false;
+            } else if (!movingRight && movingBlock.transform.position.x < tower.Peek().transform.position.x - currentWidth - blockMargen) {
+                movingRight = true;
+            }
+            movingBlock.transform.position += new Vector3(movingRight ? blockSpeed : -blockSpeed, 0, 0);
+        }
+    }
+
+    private void GetNewBlock() {
+        currentPosition += blockHeight;
+        movingBlock = Instantiate(blockPrefab, new Vector3(towerHorizontalPosition, currentPosition), Quaternion.identity, this.transform);
+        movingBlock.transform.localScale = new Vector3(currentWidth, blockHeight, 1);
+        movingBlock.GetComponent<Renderer>().material.color = color2;
+    }
+
+    private void RestartTower() {
+        foreach(GameObject go in tower) {
+            Destroy(go);
+        }
+        if(movingBlock != null) {
+            Destroy(movingBlock);
+        }
+        tower.Clear();
+        currentWidth = blockWidth;
+        currentPosition = -4.5f + (blockHeight / 2);
+        var block = Instantiate(blockPrefab, new Vector3(towerHorizontalPosition, currentPosition), Quaternion.identity, this.transform);
+        block.transform.localScale = new Vector3(blockWidth, blockHeight, 1);
+        block.GetComponent<Renderer>().material.color = color1;
+        tower.Push(block);
+    }
+}
